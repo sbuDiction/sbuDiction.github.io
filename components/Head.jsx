e70fb4c12832357e0d42a93427f648900d31f41f
sbuDiction <sibusisonkosi0897@gmail.com>
2023-01-01 19:59:31 +0200

    changed site meta

--- components/Head.jsx
+++ components/Head.jsx
@@ -10,7 +10,7 @@ const CustomHead = ({ title }) => {
       />
       <meta
         name="keywords"
-        content="Sibusiso Nkosi, kostas, georgiou, data scientist portfolio, machine learning engineer, Sibusiso Nkosi portfolio, vscode-portfolio"
+        content="Sibusiso Nkosi, full stack developer portfolio, software developer engineer, Sibusiso Nkosi portfolio, vscode-portfolio"
       />
       <meta property="og:title" content="Sibusiso Nkosi's Portfolio" />
       <meta
@@ -18,7 +18,7 @@ const CustomHead = ({ title }) => {
         content="Sibusiso Nkosi's Profolio."
       />
       <meta property="og:image" content="https://imgur.com/YTNNknY.png" />
-      <meta property="og:url" content="https://gkos.tech" />
+      <meta property="og:url" content="https://sbu-diction-github-io-ixax.vercel.app" />
       <meta name="twitter:card" content="summary_large_image" />
     </Head>
   );
